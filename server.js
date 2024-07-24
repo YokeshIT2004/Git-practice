d7abda91fecabcc74d36667a08280ee0b1d2f429
Guhanandan <guhanandan6542060@gmail.com>
2024-07-24 09:18:51 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/" , (req , res) => {
     res.json({message : "This is from node server"});
 });
 
+app.get("/hello-world" , (req ,res) => {
+    res.send("Hello World");
+});
+
 app.listen(PORT,()=>{
     console.log(`Server is running ${PORT}`);
 });
